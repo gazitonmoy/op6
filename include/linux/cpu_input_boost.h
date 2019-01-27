d8b986c6a57fbc1576161ad7cd4c99cebf330000
kdrag0n <dragon@khronodragon.com>
2019-01-26 16:19:33 -0800

    cpu_input_boost: Introduce should_kick_frame_boost function
    We will be expanding on this in the upcoming commits.
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- include/linux/cpu_input_boost.h
+++ include/linux/cpu_input_boost.h
@@ -11,6 +11,12 @@ void cpu_input_boost_kick_max(unsigned int duration_ms);
 void cluster_input_boost_kick_max(unsigned int duration_ms, int cpu);
 void cpu_input_boost_kick_general(unsigned int duration_ms);
 void cpu_input_boost_kick_flex(void);
+
+static inline bool should_kick_frame_boost(void)
+{
+	return time_before(jiffies,
+			   last_input_jiffies + msecs_to_jiffies(1750));
+}
 #else
 static inline void cpu_input_boost_kick(void)
 {
@@ -29,6 +35,11 @@ static inline void cpu_input_boost_kick_general(unsigned int duration_ms)
 }
 static inline void cpu_input_boost_kick_flex(void)
 {
+}
+
+static inline bool should_kick_frame_boost(void)
+{
+	return false;
 }
 #endif
 
