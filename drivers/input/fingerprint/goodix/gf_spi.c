9695e63d0e54a13a5fb9eab4da4e025a99e164fa
Erik Müller <erik.lars.mueller@googlemail.com>
2019-01-30 22:57:14 +0100

    Kill left over op shit

--- drivers/input/fingerprint/goodix/gf_spi.c
+++ drivers/input/fingerprint/goodix/gf_spi.c
@@ -916,21 +916,14 @@ static int gf_probe(struct platform_device *pdev)
 	status = gf_pinctrl_init(gf_dev);
 	if (status)
 		goto err_irq;
-	if (get_boot_mode() !=  MSM_BOOT_MODE__FACTORY) {
-		status = pinctrl_select_state(gf_dev->gf_pinctrl,
+	
+	status = pinctrl_select_state(gf_dev->gf_pinctrl,
 			gf_dev->gpio_state_enable);
-		if (status) {
+	if (status) {
 			pr_err("can not set %s pins\n", "fp_en_init");
 			goto error_hw;
-		}
-	} else {
-		status = pinctrl_select_state(gf_dev->gf_pinctrl,
-			gf_dev->gpio_state_disable);
-		if (status) {
-			pr_err("can not set %s pins\n", "fp_dis_init");
-			goto error_hw;
-		}
 	}
+	
 	if (status == 0) {
 		/*input device subsystem */
 		gf_dev->input = input_allocate_device();
