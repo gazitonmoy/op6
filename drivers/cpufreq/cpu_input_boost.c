d08b180723ea4a5815729288e6a4ea6eb9b6a512
Erik Müller <erik.lars.mueller@googlemail.com>
2019-02-03 15:31:08 +0100

    Revert "cpu_input_boost: [FIX] Rework scheduling setup"
    This reverts commit 1ef9cd3d216423aaafffaab598ad8de813638dc7.

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -11,7 +11,6 @@
 #include <linux/input.h>
 #include <linux/moduleparam.h>
 #include <linux/slab.h>
-#include <linux/kthread.h>
 
 static __read_mostly unsigned int input_boost_freq_lp = CONFIG_INPUT_BOOST_FREQ_LP;
 static __read_mostly unsigned int input_boost_freq_hp = CONFIG_INPUT_BOOST_FREQ_PERF;
@@ -61,15 +60,14 @@ module_param(remove_input_boost_freq_perf, uint, 0644);
 #define FLEX_BOOST		BIT(5)
 
 struct boost_drv {
-	struct kthread_worker worker;
-	struct task_struct *worker_thread;
-	struct kthread_work input_boost;
+	struct workqueue_struct *wq;
+	struct work_struct input_boost;
 	struct delayed_work input_unboost;
-	struct kthread_work max_boost;
+	struct work_struct max_boost;
 	struct delayed_work max_unboost;
-	struct kthread_work general_boost;
+	struct work_struct general_boost;
 	struct delayed_work general_unboost;
-	struct kthread_work flex_boost;
+	struct work_struct flex_boost;
 	struct delayed_work flex_unboost;
 	struct delayed_work stune_extender_unboost;
 	struct notifier_block cpu_notif;
@@ -168,7 +166,7 @@ void cpu_input_boost_kick(void)
 	if (!b)
 		return;
 
-	kthread_queue_work(&b->worker, &b->input_boost);
+	queue_work(b->wq, &b->input_boost);
 }
 
 static void __cpu_input_boost_kick_max(struct boost_drv *b,
@@ -188,7 +186,7 @@ static void __cpu_input_boost_kick_max(struct boost_drv *b,
 		new_expires) != curr_expires);
 
 	atomic_set(&b->max_boost_dur, duration_ms);
-	kthread_queue_work(&b->worker, &b->max_boost);
+	queue_work(b->wq, &b->max_boost);
 }
 
 void cpu_input_boost_kick_max(unsigned int duration_ms)
@@ -227,7 +225,7 @@ static void __cpu_input_boost_kick_general(struct boost_drv *b,
 		new_expires) != curr_expires);
 
 	atomic_set(&b->general_boost_dur, duration_ms);
-	kthread_queue_work(&b->worker, &b->general_boost);
+	queue_work(b->wq, &b->general_boost);
 }
 
 void cpu_input_boost_kick_general(unsigned int duration_ms)
@@ -261,7 +259,7 @@ static void __cpu_input_boost_kick_flex(struct boost_drv *b)
 		new_expires) != curr_expires);
 
 	atomic_set(&b->flex_boost_dur, flex_boost_duration);
-	kthread_queue_work(&b->worker, &b->flex_boost);
+	queue_work(b->wq, &b->flex_boost);
 }
 
 void cpu_input_boost_kick_flex(void)
@@ -280,7 +278,7 @@ void cpu_input_boost_kick_flex(void)
 	__cpu_input_boost_kick_flex(b);
 }
 
-static void input_boost_worker(struct kthread_work *work)
+static void input_boost_worker(struct work_struct *work)
 {
 	struct boost_drv *b = container_of(work, typeof(*b), input_boost);
 
@@ -309,7 +307,7 @@ static void input_unboost_worker(struct work_struct *work)
 	update_online_cpu_policy();
 }
 
-static void max_boost_worker(struct kthread_work *work)
+static void max_boost_worker(struct work_struct *work)
 {
 	struct boost_drv *b = container_of(work, typeof(*b), max_boost);
 
@@ -338,7 +336,7 @@ static void max_unboost_worker(struct work_struct *work)
 	update_online_cpu_policy();
 }
 
-static void general_boost_worker(struct kthread_work *work)
+static void general_boost_worker(struct work_struct *work)
 {
 	struct boost_drv *b = container_of(work, typeof(*b), general_boost);
 
@@ -366,7 +364,7 @@ static void general_unboost_worker(struct work_struct *work)
 	update_online_cpu_policy();
 }
 
-static void flex_boost_worker(struct kthread_work *work)
+static void flex_boost_worker(struct work_struct *work)
 {
 	struct boost_drv *b = container_of(work, typeof(*b), flex_boost);
 
@@ -511,7 +509,7 @@ static void cpu_input_boost_input_event(struct input_handle *handle,
 	if (!(state & SCREEN_AWAKE))
 		return;
 
-	kthread_queue_work(&b->worker, &b->input_boost);
+	queue_work(b->wq, &b->input_boost);
 }
 
 static int cpu_input_boost_input_connect(struct input_handler *handler,
@@ -590,49 +588,24 @@ static struct input_handler cpu_input_boost_input_handler = {
 static int __init cpu_input_boost_init(void)
 {
 	struct boost_drv *b;
-	int ret, i;
-	struct sched_param param = { .sched_priority = MAX_RT_PRIO - 2 };
-	cpumask_t sys_bg_mask;
+	int ret;
 
 	b = kzalloc(sizeof(*b), GFP_KERNEL);
 	if (!b)
 		return -ENOMEM;
 
-<<<<<<< HEAD
 	b->wq = alloc_workqueue("cpu_input_boost_wq", WQ_HIGHPRI, 0);
 	if (!b->wq) {
 		ret = -ENOMEM;
-=======
-	kthread_init_worker(&b->worker);
-	b->worker_thread = kthread_run(kthread_worker_fn, &b->worker,
-				       "cpu_input_boost_thread");
-	if (IS_ERR(b->worker_thread)) {
-		ret = PTR_ERR(b->worker_thread);
-		pr_err("Failed to start kworker, err: %d\n", ret);
->>>>>>> 880037420b811... cpu_input_boost: [FIX] Rework scheduling setup
 		goto free_b;
 	}
 
-	ret = sched_setscheduler(b->worker_thread, SCHED_FIFO, &param);
-	if (!ret)
-		pr_err("Failed to set SCHED_FIFO on kworker, err: %d\n", ret);
-
-	/* Init the cpumask */
-	for (i = 0; i <= 2; i++)
-		cpumask_set_cpu(i, &sys_bg_mask);
-
-	/* Bind it to the cpumask */
-	kthread_bind_mask(b->worker_thread, &sys_bg_mask);
-
-	/* Wake it up */
-	wake_up_process(b->worker_thread);
-
 	atomic64_set(&b->max_boost_expires, 0);
-	kthread_init_work(&b->input_boost, input_boost_worker);
+	INIT_WORK(&b->input_boost, input_boost_worker);
 	INIT_DELAYED_WORK(&b->input_unboost, input_unboost_worker);
-	kthread_init_work(&b->max_boost, max_boost_worker);
+	INIT_WORK(&b->max_boost, max_boost_worker);
 	INIT_DELAYED_WORK(&b->max_unboost, max_unboost_worker);
-	kthread_init_work(&b->general_boost, general_boost_worker);
+	INIT_WORK(&b->general_boost, general_boost_worker);
 	INIT_DELAYED_WORK(&b->general_unboost, general_unboost_worker);
 	INIT_WORK(&b->flex_boost, flex_boost_worker);
 	INIT_DELAYED_WORK(&b->flex_unboost, flex_unboost_worker);
@@ -658,7 +631,7 @@ static int __init cpu_input_boost_init(void)
 	b->msm_drm_notif.priority = INT_MAX;
 	ret = msm_drm_register_client(&b->msm_drm_notif);
 	if (ret) {
-		pr_err("Failed to register msm_drm_notifier, err: %d\n", ret);
+		pr_err("Failed to register dsi_panel_notifier, err: %d\n", ret);
 		goto unregister_handler;
 	}
 
@@ -673,7 +646,7 @@ unregister_handler:
 unregister_cpu_notif:
 	cpufreq_unregister_notifier(&b->cpu_notif, CPUFREQ_POLICY_NOTIFIER);
 destroy_wq:
-	kthread_destroy_worker(&b->worker);
+	destroy_workqueue(b->wq);
 free_b:
 	kfree(b);
 	return ret;
