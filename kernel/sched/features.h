3073de6cb6b74b6a464d22319fc73221296cdc45
Erik Müller <erik.lars.mueller@googlemail.com>
2019-02-24 21:19:44 +0100

    Revert "Revert "sched/fair: Add bias schedtune boosted tasks sched feature""
    This reverts commit e6bb1949552180da67b5bf1b3a5ed1c7855001ed.

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -114,4 +114,10 @@ SCHED_FEAT(EAS_USE_NEED_IDLE, true)
  * If disabled, this behaviour will only apply to tasks of the
  * RT class.
  */
-SCHED_FEAT(SCHEDTUNE_BOOST_HOLD_ALL, true)
+SCHED_FEAT(SCHEDTUNE_BOOST_HOLD_ALL, false)
+
+/*
+ * Bias schedtune boosted tasks to higher capacity CPUs.
+ * If disabled, no bias will be applied.
+ */
+SCHED_FEAT(STUNE_BOOST_BIAS_BIG, true)
