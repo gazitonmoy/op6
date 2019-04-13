3d040628145cfe926a4e1ad16012b8bed7e59ae7
Erik Müller <erik.lars.mueller@googlemail.com>
2019-04-14 00:48:41 +0200

    GPU: 844MHz 787 MHz

--- drivers/clk/qcom/gpucc-sdm845.c
+++ drivers/clk/qcom/gpucc-sdm845.c
@@ -284,7 +284,7 @@ static const struct freq_tbl  ftbl_gpu_cc_gx_gfx3d_clk_src_sdm845_v2[] = {
 	F(596000000, P_CRC_DIV,  1, 0, 0),
 	F(636000000, P_CRC_DIV,  1, 0, 0),
 	F(710000000, P_CRC_DIV,  1, 0, 0),
-	F(767000000, P_CRC_DIV,  1, 0, 0),
+	F(787000000, P_CRC_DIV,  1, 0, 0),
 	F(844000000, P_CRC_DIV,  1, 0, 0),
 	{ }
 };
@@ -645,7 +645,7 @@ static void gpu_cc_gfx_sdm845_fixup_sdm845v2(void)
 		596000000;
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH] = 636000000;
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH_1] = 710000000;
-	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH_2] = 767000000;
+	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH_2] = 787000000;
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH_L1] =
 		844000000;	;
 }
