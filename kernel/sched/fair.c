0a156a21a1521bf44ae69d0aed3137121ed18f2b
Maria Yu <aiquny@codeaurora.org>
2019-05-15 11:45:50 +0800

    sched/fair: Consider others if target cpu overutilized
    If target cpu overutilized, it's better to consider
    other group cpu. It can avoid unnecessary waiting on
    overutilized cpu and wait until load balance for task
    to be run.
    
    Change-Id: I6f8bccb611d2f11471254cf2795fb5bf3f122292
    Signed-off-by: Maria Yu <aiquny@codeaurora.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6240,6 +6240,12 @@ static inline bool task_fits_max(struct task_struct *p, int cpu)
 	return __task_fits(p, cpu, 0);
 }
 
+static inline bool cpu_check_overutil_condition(int cpu,
+						unsigned long util)
+{
+	return (capacity_orig_of(cpu) * 1024) < (util * capacity_margin);
+}
+
 bool __cpu_overutilized(int cpu, int delta)
 {
 	return (capacity_orig_of(cpu) * 1024) <
@@ -7110,6 +7116,8 @@ retry:
 			if (idle_cpu(i))
 				idle_idx = idle_get_state_idx(cpu_rq(i));
 
+			if (cpu_check_overutil_condition(i, new_util))
+				continue;
 
 			/*
 			 * Case A) Latency sensitive tasks
