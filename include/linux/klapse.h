ef8e5162918136ee3047e6fde1731af1e0da0c9c
tanish2k09 <tanish2k09.dev@gmail.com>
2019-06-12 21:29:31 +0200

    Update K-Lapse 5.0

--- include/linux/klapse.h
+++ include/linux/klapse.h
@@ -1,46 +1,46 @@
-#ifndef _LINUX_KLAPSE_H
-#define _LINUX_KLAPSE_H
+#ifndef _KLAPSE_H
+#define _KLAPSE_H
 
-/* Required variables for external access. Change as per use */
-extern void set_rgb_slider(u32 bl_lvl);
+/* KLAPSE_MDSS : Use 1 if using with MDSS */
+#define KLAPSE_MDSS 0
 
-// This file uses generalised K_### defines
-// The interpretation (right argument) should be the respective color's var
-#define K_RED    kcal_red
-#define K_GREEN  kcal_green
-#define K_BLUE   kcal_blue
+/* set_rgb_slider : Interface function for brightness-mode */
+typedef u32 bl_type_t;
+extern void set_rgb_slider(bl_type_t bl_lvl);
 
-#define K_TYPE   unsigned short
+/* Variable type for rgb */
+typedef unsigned short col_type_t;
 
-extern K_TYPE K_RED, K_GREEN, K_BLUE;
+#if KLAPSE_MDSS
+ #define K_RED    kcal_get_color(0)
+ #define K_GREEN  kcal_get_color(1)
+ #define K_BLUE   kcal_get_color(2)
 
-/* DEFAULT_ENABLE values :
- * 0 = off
- * 1 = time-based scaling
- * 2 = brightness-based scaling
- */
-#define DEFAULT_ENABLE  0
+ extern col_type_t kcal_get_color(unsigned short int code);
+ extern void klapse_kcal_push(int r, int g, int b);
+#else
+ #define K_RED    kcal_red
+ #define K_GREEN  kcal_green
+ #define K_BLUE   kcal_blue
 
-// MAX_SCALE : Maximum value of RGB possible
-#define MAX_SCALE       256
+ extern col_type_t K_RED, K_GREEN, K_BLUE;
+#endif
 
-// SCALE_VAL_MIN : Minimum value of RGB recommended
-#define SCALE_VAL_MIN   20
+/* Constants - Customize as needed */
+#define DEFAULT_ENABLE 0 /* 0 = off, 1 = time-based, 2 = brightness-based */
 
-// MAX_BRIGHTNESS : Maximum value of the display brightness/backlight
-#define MAX_BRIGHTNESS  1023
+#define MAX_SCALE 256 /* Maximum value of RGB possible */
 
-// MIN_BRIGHTNESS : Minimum value of the display brightness/backlight
-#define MIN_BRIGHTNESS  2
+#define MIN_SCALE 20 /* Minimum value of RGB recommended */
 
-/* UPPER_BL_LVL : Initial upper limit for brightness-dependent mode. 
- * Value <= MAX_BRIGHTNESS && > LOWER_BL_LVL (MUST)
- */
-#define UPPER_BL_LVL  200
+#define MAX_BRIGHTNESS 1023 /* Maximum display brightness */
 
-/* LOWER_BL_LVL : Initial lower limit for brightness-dependent mode. 
- * Value < UPPER_BL_LVL (MUST)
- */
-#define LOWER_BL_LVL 2
+#define MIN_BRIGHTNESS 2 /* Minimum display brightness */
 
-#endif  /* _LINUX_KLAPSE_H */
+#define UPPER_BL_LVL 400 /* Upper target for brightness-dependent mode */
+
+#define LOWER_BL_LVL 2 /* Lower target for brightness-dependent mode */
+
+#define DEFAULT_FLOW_FREQ 360 /* Flow delays for rapid pushes in mode 2 */
+
+#endif  /* _KLAPSE_H */
