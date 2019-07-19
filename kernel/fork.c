668beaabf5714855712f0b2be226e7f7d77c5035
Erik Müller <erik.lars.mueller@googlemail.com>
2019-07-20 00:17:09 +0200

    fork: reduce devfreq boost time

--- kernel/fork.c
+++ kernel/fork.c
@@ -2010,7 +2010,7 @@ long _do_fork(unsigned long clone_flags,
 		if (task_is_zygote(current)) {
 			cpu_input_boost_kick_cluster1(750);
 			cpu_input_boost_kick_cluster2(750);
-			devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 750);
+			devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 100);
 		}
 	}
 #endif	
