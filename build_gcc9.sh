348d132b08c74f862c303c60681fbe9eeae7372f
Erik Müller <erik.lars.mueller@googlemail.com>
2019-09-07 23:30:42 +0200

    Version 2.0.76

--- build_gcc9.sh
+++ build_gcc9.sh
@@ -22,7 +22,7 @@ export CROSS_COMPILE_ARM32=${HOME}/android/Toolchains/gcc9eabi_92/bin/arm-eabi-
 DEFCONFIG="smurf_defconfig"
 
 # Kernel Details
-VER=".2.0.75"
+VER=".2.0.76"
 
 # Paths
 KERNEL_DIR=`pwd`
